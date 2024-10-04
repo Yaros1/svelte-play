dd92a80a9f1a36384a755f3059ca5a891b6a7e78
Yaros1 <Yaros@live.com>
2024-10-04 23:57:31 +0300

    Add files via upload

--- my-component.js
+++ my-component.js
@@ -1,68 +1,70 @@
-import { SvelteComponent, init, safe_not_equal, element, text, insert, append, listen, set_data, noop, detach } from "svelte/internal";
-const PUBLIC_VERSION = "4";
-if (typeof window !== "undefined")
-  (window.__svelte || (window.__svelte = { v: /* @__PURE__ */ new Set() })).v.add(PUBLIC_VERSION);
-function create_fragment(ctx) {
-  let button;
-  let t0;
-  let t1;
-  let mounted;
-  let dispose;
-  return {
-    c() {
-      button = element("button");
-      t0 = text("count is ");
-      t1 = text(
-        /*count*/
-        ctx[0]
-      );
-    },
-    m(target, anchor) {
-      insert(target, button, anchor);
-      append(button, t0);
-      append(button, t1);
-      if (!mounted) {
-        dispose = listen(
-          button,
-          "click",
-          /*increment*/
-          ctx[1]
+(function(global, factory) {
+  typeof exports === "object" && typeof module !== "undefined" ? module.exports = factory(require("svelte/internal")) : typeof define === "function" && define.amd ? define(["svelte/internal"], factory) : (global = typeof globalThis !== "undefined" ? globalThis : global || self, global.MyComponent = factory(global["svelte/internal"]));
+})(this, function(internal) {
+  "use strict";
+  const PUBLIC_VERSION = "4";
+  if (typeof window !== "undefined")
+    (window.__svelte || (window.__svelte = { v: /* @__PURE__ */ new Set() })).v.add(PUBLIC_VERSION);
+  function create_fragment(ctx) {
+    let button;
+    let t0;
+    let t1;
+    let mounted;
+    let dispose;
+    return {
+      c() {
+        button = internal.element("button");
+        t0 = internal.text("count is ");
+        t1 = internal.text(
+          /*count*/
+          ctx[0]
         );
-        mounted = true;
+      },
+      m(target, anchor) {
+        internal.insert(target, button, anchor);
+        internal.append(button, t0);
+        internal.append(button, t1);
+        if (!mounted) {
+          dispose = internal.listen(
+            button,
+            "click",
+            /*increment*/
+            ctx[1]
+          );
+          mounted = true;
+        }
+      },
+      p(ctx2, [dirty]) {
+        if (dirty & /*count*/
+        1) internal.set_data(
+          t1,
+          /*count*/
+          ctx2[0]
+        );
+      },
+      i: internal.noop,
+      o: internal.noop,
+      d(detaching) {
+        if (detaching) {
+          internal.detach(button);
+        }
+        mounted = false;
+        dispose();
       }
-    },
-    p(ctx2, [dirty]) {
-      if (dirty & /*count*/
-      1) set_data(
-        t1,
-        /*count*/
-        ctx2[0]
-      );
-    },
-    i: noop,
-    o: noop,
-    d(detaching) {
-      if (detaching) {
-        detach(button);
-      }
-      mounted = false;
-      dispose();
-    }
-  };
-}
-function instance($$self, $$props, $$invalidate) {
-  let count = 0;
-  const increment = () => {
-    $$invalidate(0, count += 1);
-  };
-  return [count, increment];
-}
-class MyComponent extends SvelteComponent {
-  constructor(options) {
-    super();
-    init(this, options, instance, create_fragment, safe_not_equal, {});
+    };
   }
-}
-export {
-  MyComponent as default
-};
+  function instance($$self, $$props, $$invalidate) {
+    let count = 0;
+    const increment = () => {
+      $$invalidate(0, count += 1);
+    };
+    return [count, increment];
+  }
+  class MyComponent extends internal.SvelteComponent {
+    constructor(options) {
+      super();
+      internal.init(this, options, instance, create_fragment, internal.safe_not_equal, {});
+    }
+  }
+  return MyComponent;
+});
